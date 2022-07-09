e32ade31955c6e39595eb63b5b3683a2867629f2
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-09 11:07:10 -0600

    Finishied the array while

--- js/app.js
+++ js/app.js
@@ -20,3 +20,17 @@ while(counter_even <= 100) {
     counter_even += 1;
 }
 
+let players_name = [`Neymar`, `Messi`, `Cristiano`];
+let players_score = [92, 125, 140];
+let players_injured = [true, false, false];
+
+let counter_players = 0;
+while(counter_players < players_name.length) {
+    
+    if(!players_injured[counter_players]) {
+        console.log(players_name[counter_players]);
+        console.log(players_score[counter_players]);
+    }
+    counter_players += 1;
+}
+
