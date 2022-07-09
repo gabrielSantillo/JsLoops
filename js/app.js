70fb429467a85a2dabeb299977301b0cabe74979
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-09 10:21:33 -0600

    Finished the knowledge check

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let counter = 0;
+while(counter <= 50){
+    console.log(counter);
+    counter = counter + 1;
+}
+
+
+let counter_negative = 0;
+while(counter_negative >= -50){
+    console.log(counter_negative);
+    counter_negative = counter_negative - 1;
+}
+
+let counter_even = 0;
+while(counter_even <= 100) {
+    console.log(counter_even);
+    counter_even = counter_even + 2;
+}
